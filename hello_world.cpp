d9231617dbf0727a655f6836a968b342a6ada884
KovalMukola <kovalmukola220@gmail.com>
2022-11-05 18:34:05 +0200

    12 Laboratorna

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"Please write your name: ";
+    cin>>name;
+    cout<<"Hello world from "<<name;
+    return 0;
+}
